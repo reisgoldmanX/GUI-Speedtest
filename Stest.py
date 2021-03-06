86d2875fa3a26585aa01105194a392ba68012a87
reisX <46068107+reisgoldmanX@users.noreply.github.com>
2021-03-06 15:16:16 +0300

    Updated
    fixed thrading and added a progress bar

--- Stest.py
+++ Stest.py
@@ -1,40 +1,52 @@
 import speedtest
 from tkinter import *
+from tkinter import ttk
+import threading
 
 
 root = Tk()  # Calling the Tkinter module.
 root.configure(background="black")  # Configure the background as black.
-root.title("STest")  # Set the Title of the program.
+root.title("SpeedTester")  # Set the Title of the program.
 root.geometry("275x275")  # Sets the resolution to (275x275)
+root.resizable(False, False)  # Set resize to false
 
-st = Label(root, text="(Do not touch anything after clicking the button!.?)", fg="blue", bg="yellow")
-st.pack()  # The warning on the GUİ.
+dev = Label(root, text="Developer : reisgoldmanX", fg="red", bg="black")
+dev.place(relx=1.0, rely=1.0, anchor='se')
+bar = ttk.Progressbar(root, orient=HORIZONTAL, length=150, mode="determinate")  # Set progress bar
+start_test = Label(root, text="(Results of the  connection speed test.)", fg="green", bg="black")
 
 
 def check():
+
     try:
-        starttest = Label(root, text="(Results of the  connection speed test.)", fg="red", bg="black")
-        starttest.pack()
         servernames = []
+        bar["value"] -= bar["value"]
         test = speedtest.Speedtest()
-        down = test.download()
-        up = test.upload()
+        bar.pack()
+        bar["value"] += 20
+        down = test.download() / (1024 * 1024)
+        bar["value"] += 30
+        up = test.upload() / (1024 * 1024)
+        bar["value"] += 30
         test.get_servers(servernames)
-        rd = ('Download Speed: {:5.2f} Mb'.format(down / (1024 * 1024)))  # Converts bits to megabytes.
-        ru = ('Upload Speed: {:5.2f} Mb'.format(up / (1024 * 1024)))  # Converts bits to megabytes.
-        res_down = Label(root, text=rd, bg="black", fg="white")  # Printing results to screen.
-        res_up = Label(root, text=ru, bg="black", fg="white")  # Printing results to screen.
-        res_ping = Label(root, text=f"ping: {test.results.ping} ms", bg="black", fg="white")
-        res_down.pack()
-        res_up.pack()
-        res_ping.pack()
+        bar["value"] += 10
+        ping = test.results.ping
+        bar["value"] += 10
+        start_test.pack()
+
+        test_done = "Download Speed: {:5.2f} Mb\nUpload Speed: {:5.2f} Mb\nPing: {} ms".format(down, up, ping)
+        all1 = Label(root, text=test_done, bg="black", fg="white", font=("Courier", 12))  # Printing results to screen.
+
+        all1.pack()
+        bar.destroy()
     except:
         err = Label(root, text="Error while testing connection speed!", fg="black", bg="red")
         err.pack()  # Sets the exception error
-    
 
-testBt = Button(root, text="Test Speed!", padx=40, pady=20, bg="black", fg="yellow", command=check)
+
+th = threading.Thread(target=check)
+testBt = Button(root, text="Test Speed!", padx=40, pady=20, bg="black", fg="yellow", command=th.start)
 testBt.pack()  # Sets the button
 
-if __name__ == "__main__":
-    root.mainloop()
+
+root.mainloop()
